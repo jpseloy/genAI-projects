e318fa133d4d8e136bca0f5c9c1d671a6e8603e8
jpseloy <79265124+jpseloy@users.noreply.github.com>
2026-07-06 21:55:22 -0300

    update projects

---
+++ genAI-essay-evaluator/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from genai-essay-evaluator!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ genAI-text-summarizer/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from genai-text-summarizer!")
+
+
+if __name__ == "__main__":
+    main()
